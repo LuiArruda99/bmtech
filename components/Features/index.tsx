61490f1fce739ce60525c647193045fe7b89b363
Luiz Arruda <luizclaudioarruda@outlook.com>
2023-06-17 22:07:31 -0300

    Initial commit

---
+++ components/Features/index.tsx
@@ -0,0 +1,30 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section
+        id="features"
+        className="bg-primary/[.03] py-16 md:py-20 lg:py-28"
+      >
+        <div className="container">
+          <SectionTitle
+            title="Soluções"
+            paragraph="Através da nossa plataforma de IA médica, estamos transformando a forma como os pacientes são diagnosticados e tratados, impactando positivamente a qualidade de vida de pessoas de todas as classes sociais."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
